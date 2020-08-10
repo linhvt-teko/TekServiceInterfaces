d2851f7e398ab09346d7f3045ae377ea854be7d1
Tung Nguyen <xtungit@gmail.com>
2020-08-10 09:47:17 +0700

    CrmService

---
+++ TekServiceInterfaces/Classes/CrmService/Models/ICrmPayload.swift
@@ -0,0 +1,17 @@
+//
+//  ICrmPayload.swift
+//  TekServiceInterfaces
+//
+//  Created by Tung Nguyen on 8/10/20.
+//
+
+import Foundation
+
+public protocol ICrmPayload {
+    var name: String? { get }
+    var phone: String? { get }
+    var province: String? { get }
+    var district: String? { get }
+    var ward: String? { get }
+    var contacts: [ICrmContact] { get }
+}
