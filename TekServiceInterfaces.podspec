f2d063842110c72fe07da6a3b64182d78ade89d9
Tung Nguyen <xtungit@gmail.com>
2020-08-11 16:55:11 +0700

    Update ICAtalogSeller

--- TekServiceInterfaces.podspec
+++ TekServiceInterfaces.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'TekServiceInterfaces'
-  s.version          = '0.1.10'
+  s.version          = '0.1.12'
   s.summary          = 'A short description of TekServiceInterfaces.'
 
 # This description is used to generate tags and improve search results.
--- TekServiceInterfaces/Classes/TekCatalogService/Models/ICatalogSeller.swift
+++ TekServiceInterfaces/Classes/TekCatalogService/Models/ICatalogSeller.swift
@@ -10,4 +10,12 @@ import Foundation
 public protocol ICatalogSeller {
     var id: Int { get }
     var displayName: String? { get }
+    var enterpriseCode: String? { get }
+    var code: String? { get }
+    var address: String? { get }
+    var englishName: String? { get }
+    var contractNo: String? { get }
+    var name: String? { get }
+    var taxNumber: String? { get }
+    var status: Int { get }
 }
