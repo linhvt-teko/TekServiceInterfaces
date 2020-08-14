fa0d9511e480b331f9796e954c369d18267b0c32
linhvt <linh.vt@teko.vn>
2020-08-14 16:32:00 +0700

    - minor

--- TekServiceInterfaces/Classes/ListingService/IKeywordsService.swift
+++ TekServiceInterfaces/Classes/ListingService/IKeywordsService.swift
@@ -10,6 +10,6 @@ import Foundation
 public typealias KeywordsResponseHandler = (_ keywords: [IKeyword], _ isSuccess: Bool) -> ()
 
 public protocol IKeywordsService {
-    func getPopularKeywords(visitorId: String?, handler: @escaping KeywordsResponseHandler)
-    func searchKeywords(query: String, visitorId: String?, page: Int, handler: @escaping KeywordsResponseHandler)
+    func getPopularKeywords(forUser userId: String?, handler: @escaping KeywordsResponseHandler)
+    func searchKeywords(query: String, forUser userId: String?, page: Int, handler: @escaping KeywordsResponseHandler)
 }
--- TekServiceInterfaces/Classes/ListingService/IRecommendationService.swift
+++ TekServiceInterfaces/Classes/ListingService/IRecommendationService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public protocol IRecommendationService {
-    func getRecommendProductsForUser(page: Int, handler: @escaping ProductsListDetailResponseHandler)
-    func getRecommendProductsForUserByCategory(page: Int, sellerCategories: [Int], handler: @escaping ProductsListDetailResponseHandler)
-    func getRecommendProductsBySku(_ sku: String, page: Int, handler: @escaping ProductsListDetailResponseHandler)
+    func getRecommendProducts(forUser userId: String?, page: Int, handler: @escaping ProductsListDetailResponseHandler)
+    func getRecommendProducts(forUser userId: String?, byCategories sellerCategories: [Int], page: Int, handler: @escaping ProductsListDetailResponseHandler)
+    func getRecommendProducts(bySku sku: String, page: Int, handler: @escaping ProductsListDetailResponseHandler)
 }
